c2c538cb064e9fe27e45b0f6afc927a1a3eb5c96
riley <rdance96@hotmail.com>
2023-05-20 09:51:59 -0500

    About to test looping my program

--- js/rps.js
+++ js/rps.js
@@ -1,5 +1,4 @@
 let n = Math.floor(Math.random() * 3);
-
 function getComputerChoice() {
     if(n == 0) {
         return("Rock");
@@ -14,7 +13,6 @@ function getComputerChoice() {
 
 const playerSelection = prompt("Rock, Paper, or Scissors?", " ").toLowerCase();
 const computerSelection = getComputerChoice().toLowerCase();
-
 function playRound() {
     if(playerSelection === computerSelection) {
         return("It's a draw! " + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()) +  " ties with " + (computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1).toLowerCase()))
@@ -26,5 +24,15 @@ function playRound() {
         return("Loser! " + (computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1).toLowerCase()) + " beats " + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()))
     }
 }
-
 alert(playRound());
+
+let s = 1;
+function roundScore () {
+    if(playRound().includes("It's a draw!") || playRound().includes("Winner!") || playRound().includes("Loser!")) {
+       return(s++); 
+    } else {
+        return("Nothing");
+    }
+}
+
+console.log(roundScore());
