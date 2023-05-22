5bf5d57b8f56879543fcae0bd927b9d99af78404
riley <rdance96@hotmail.com>
2023-05-22 12:09:38 -0500

    Reworked program and it's fully functioning

--- js/rps.js
+++ js/rps.js
@@ -1,40 +1,56 @@
-let scoreTotal = 0;
-for (; scoreTotal < 5; scoreTotal++) {
+let computerSelection;
+let playerSelection;
+let computerScore = 0;
+let playerScore = 0;
+let roundTotal = 0;
+
+for (; roundTotal < 5 ; ++roundTotal) {
 function getComputerChoice() {
     let n = Math.floor(Math.random() * 3);
-    if(n == 0) {
-        return("Rock");
-    } else if(n == 1) {
-        return("Paper");
-    } else if(n == 2) {
-        return("Scissors");
-    } else {
-        return("What happened?");
-    }
+    if (n === 0) {
+        return("rock");
+    } else if (n === 1) {
+        return("paper");
+    } else if (n === 2) {
+        return("scissors");
+    } else {}
+    
 }
 
+computerSelection = getComputerChoice();
+playerSelection = prompt("Rock, Paper, or Scissors?", " ").toLowerCase();
+
 function playRound() {
-    const playerSelection = prompt("Rock, Paper, or Scissors?", " ").toLowerCase();
-    const computerSelection = getComputerChoice().toLowerCase();
-    if(playerSelection === computerSelection) {
-        return("It's a draw! " + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()) +  " ties with " + (computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1).toLowerCase()))
-    } else if(playerSelection === "paper" && computerSelection === "rock" 
-    || playerSelection === "rock" && computerSelection === "scissors" 
-    || playerSelection === "scissors" && computerSelection === "paper") {
-        return("Winner! " + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()) + " beats " + (computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1).toLowerCase()))
-    } else {
-        return("Loser! " + (computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1).toLowerCase()) + " beats " + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()))
-    }
+    if (playerSelection === computerSelection) {
+        alert("It's a tie!");
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        playerScore = ++playerScore;
+        alert("Winner! " + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()) + " beats " + (computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1).toLowerCase()));
+    } else if (
+        (computerSelection === "rock" && playerSelection === "scissors") ||
+        (computerSelection === "paper" && playerSelection === "rock") ||
+        (computerSelection === "scissors" && playerSelection === "paper")
+    ) {
+        computerScore = ++computerScore;
+        alert("Loser! " + (computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1).toLowerCase()) + " beats " + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()));
+    } else {}
 }
-alert(playRound());
-console.log(scoreTotal);
+playRound();
+console.log(playerScore);
+console.log(computerScore);
+
+function game() {
+    if (roundTotal === 4 && playerScore === computerScore) {
+        alert("Tie Game! Final Score is Player " + playerScore + " Computer " + computerScore)
+    } else if (roundTotal === 4 && playerScore > computerScore) {
+        alert("Player beats Computer! Final Score is Player " + playerScore + " Computer " + computerScore)
+    } else if (roundTotal === 4 && computerScore > playerScore) {
+        alert("Computer beats Player! Final Score is Player " + playerScore + " Computer " + computerScore)
+    } else {}
 }
-/*function roundScore () {
-    let s = 1;
-    if(playRound().includes("It's a draw!") || playRound().includes("Winner!") || playRound().includes("Loser!")) {
-       return(s++); 
-    } else {
-        return("Nothing");
-    }
+game();
 }
-console.log(roundScore())*/
